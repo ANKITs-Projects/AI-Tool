082ce40ab74a9f70ee0001e9e7d169046be9dc49
ANKITs-Projects <ankitgupta0429@gmail.com>
2026-03-12 10:37:13 +0530

    Now the version 1.0.0 is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import "./App.css";
-import { Answer } from "./components/Answer";
 import { QnA } from "./components/QnA";
+import { MdDelete } from "react-icons/md";
+
 const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 const URI = import.meta.env.VITE_URI;
 
@@ -18,6 +19,9 @@ function App() {
       return [];
     }
   });
+
+  const textareaRef = useRef(null);
+  const bottomRef = useRef(null);
 
   const payload = {
     contents: [
@@ -52,8 +56,8 @@ function App() {
   };
 
   const askQuery = async () => {
+    if(!query) return
     try {
-      setQuery("")
       addToHistory();
       const response = await fetch(URI + API_KEY, {
         method: "POST",
@@ -69,7 +73,8 @@ function App() {
         let response = data.candidates[0].content.parts[0].text;
         response = response.split("* ").map((item) => item.trim());
         const res = { query, response };
-        setResult([...result, res]);
+        setResult([...result, res]);     
+        setQuery("");
       } else {
         console.log("Unexpected response format:", data);
       }
@@ -78,31 +83,61 @@ function App() {
     }
   };
 
+  const clearHistory = () => {
+    localStorage.clear("history");
+    setResentHistory([]);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      askQuery();
+    }
+  };
+
+useEffect(() => {
+  bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+}, [result]);
+
   useEffect(() => {
-    setResult([
-      {
-        query: "Hello how are you",
-        response: ["Hey what's up how can i hellp you"],
-      },
-      { query: "Hello", response: ["Hey what's up how can i hellp you"] },
-    ]);
-  }, []);
+    const el = textareaRef.current;
+    if (!el) return;
+
+    el.style.height = "auto";
+    el.style.height = el.scrollHeight + "px";
+  }, [query]);
+
+  // useEffect(() => {
+  //   setResult([
+  //     {
+  //       query: "Hello how are you",
+  //       response: ["Hey what's up how can i hellp you"],
+  //     },
+  //     { query: "Hello", response: ["Hey what's up how can i hellp you"] },
+  //   ]);
+  // }, []);
 
   return (
     <div className="grid grid-cols-5 h-screen text-center">
       <div className="col-span-1 bg-zinc-700">
-        <ul>
+        <h1 className="text-zinc-200 text-lg flex justify-center gap-3">
+          <span>Recent searches</span>
+          <MdDelete className="my-auto cursor-pointer" onClick={clearHistory} />
+        </h1>
+        <ul className="text-left text-zinc-300 px-2 mt-4 overflow-auto text-md">
           {Array.isArray(resentHistory) && resentHistory.length > 0 ? (
             resentHistory.map((ele, i) => (
               <li
                 key={i}
-                className="p-2 hover:bg-zinc-800 rounded cursor-pointer"
+                className="p-2 hover:bg-zinc-800 rounded cursor-pointer truncate"
               >
-                {ele}
+                {ele.replace(/\b\w/g, (char) => char.toUpperCase())}
               </li>
             ))
           ) : (
-            <p className="text-zinc-500 text-sm">No recent searches</p>
+            <h1 className="bg-zinc-600 text-center rounded font-bold my-auto py-1.5 text-sm ">
+              No Recent Searches
+            </h1>
           )}
         </ul>
       </div>
@@ -114,7 +149,7 @@ function App() {
           </h1>
           <div className="">
             <ul>
-              {result.length &&
+              {result.length > 0 &&
                 result.map((qna, i) => {
                   return (
                     <ul key={Date.now() + i}>
@@ -123,19 +158,26 @@ function App() {
                   );
                 })}
             </ul>
+            <div ref={bottomRef}></div>
           </div>
         </div>
 
-        <div className=" flex justify-center items-center h-24">
-          <div className="bg-zinc-700 w-1/2 text-zinc-300 p-1 pr-5 h-16 rounded-4xl border-zinc-600 border flex">
-            <input
-              type="text"
+        <div className="flex justify-center">
+          <div className="bg-zinc-700 w-1/2 text-zinc-300 px-3 pr-4 min-h-15 rounded-3xl border border-zinc-600 flex items-end">
+            <textarea
+              ref={textareaRef}
               placeholder="Ask me anything"
               value={query}
+              rows={1}
+              onKeyDown={handleKeyDown}
               onChange={(e) => setQuery(e.target.value)}
-              className="w-full h-full p-3 outline-none"
+              className="flex-1 resize-none outline-none bg-transparent px-3 py-3 leading-6 max-h-50 overflow-y-auto"
             />
-            <button onClick={askQuery} className="cursor-pointer">
+
+            <button
+              onClick={askQuery}
+              className="cursor-pointer self-center bg-zinc-600 hover:bg-zinc-500 px-4 py-1 rounded-full"
+            >
               Ask
             </button>
           </div>
